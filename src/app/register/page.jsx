3bb16e6500c3534a60d8e56789545baef72addd8
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-09-29 01:44:30 -0300

    Update page.jsx

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,109 +1,100 @@
 function Register() {
   return (
-    <section class="">
-      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+    <section className="">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
-          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 "
+          className="flex items-center mb-6 text-2xl font-semibold text-gray-900 "
         >
-          <img class="w-40" src="/img/logo.png" alt="logo" />
+          <img className="w-40" src="/img/logo.png" alt="logo" />
         </a>
-        <div class="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-              Create and account
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+              Create an account
             </h1>
-            <form class="6 grid grid-cols-2" action="#">
-              <div class="relative mb-6" data-te-input-wrapper-init>
-                <input
-                  type="text"
-                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
-                  id="exampleFormControlInput2"
-                />
-                <label
-                  for="exampleFormControlInput2"
-                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
-                >
-                  Email address
-                </label>
+            <form className="space-y-4" action="#">
+              <div className="mb-6 flex space-x-4">
+                <div className="relative" data-te-input-wrapper-init>
+                  <input
+                    type="text"
+                    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none"
+                    id="firstName"
+                    placeholder="First Name"
+                  />
+                </div>
+                <div className="relative" data-te-input-wrapper-init>
+                  <input
+                    type="text"
+                    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none"
+                    id="lastName"
+                    placeholder="Last Name"
+                  />
+                </div>
               </div>
-              <div class="relative mb-6" data-te-input-wrapper-init>
-                <input
-                  type="text"
-                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
-                  id="exampleFormControlInput2"
-                />
-                <label
-                  for="exampleFormControlInput2"
-                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
-                >
-                  Email address
-                </label>
+              <div className="mb-6">
+                <div className="relative" data-te-input-wrapper-init>
+                  <input
+                    type="text"
+                    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none"
+                    id="email"
+                    placeholder="Email address"
+                  />
+                </div>
               </div>
-              <div class="relative mb-6" data-te-input-wrapper-init>
-                <input
-                  type="text"
-                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
-                  id="exampleFormControlInput2"
-                />
-                <label
-                  for="exampleFormControlInput2"
-                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
-                >
-                  Email address
-                </label>
+              <div className="mb-6">
+                <div className="relative" data-te-input-wrapper-init>
+                  <input
+                    type="text"
+                    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none"
+                    id="phoneNumber"
+                    placeholder="Phone Number"
+                  />
+                </div>
               </div>
-              <div class="relative mb-6" data-te-input-wrapper-init>
-                <input
-                  type="text"
-                  class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none "
-                  id="exampleFormControlInput2"
-                />
-                <label
-                  for="exampleFormControlInput2"
-                  class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none "
-                >
-                  Email address
-                </label>
-              </div>
-            </form>{" "}
-            <div class="flex items-start">
-              <div class="flex items-center h-5">
+            </form>
+            <div className="flex items-start flex-col">
+              <div className="mb-3 flex items-center h-5">
                 <input
                   id="terms"
                   type="checkbox"
-                  class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
+                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
                   required=""
                 />
-              </div>
-              <div class="ml-3 text-sm">
-                <label for="terms" class="font-light text-gray-500 ">
+                <label
+                  htmlFor="terms"
+                  className="ml-2 font-light text-gray-500"
+                >
                   I accept the
                   <a
-                    class="font-medium text-primary-600 hover:underline "
+                    className="font-medium text-primary-600 hover:underline "
                     href="#"
                   >
                     Terms and Conditions
                   </a>
                 </label>
               </div>
+              <button
+                type="submit"
+                className="w-full bg-gray-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              >
+                Create an account
+              </button>
+              <p className="mt-3 text-sm font-light text-gray-500">
+                Already have an account?
+                <a
+                  href="#"
+                  className="font-medium text-primary-600 hover:underline "
+                >
+                  Login here
+                </a>
+              </p>
             </div>
-            <button
-              type="submit"
-              class="w-full  bg-gray-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
-            >
-              Create an account
-            </button>
-            <p class="text-sm font-light text-gray-500 ">
-              Already have an account?
-              <a href="#" class="font-medium text-primary-600 hover:underline ">
-                Login here
-              </a>
-            </p>
           </div>
         </div>
       </div>
     </section>
   );
 }
+
 export default Register;
