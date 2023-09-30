46bb12d2e2a285a13e6d94155b284732c8b6eea1
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-09-29 21:54:01 -0300

    s

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,4 +1,69 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCzD--npY_6fZcXH-8CzBV7UGzPBqg85y8",
+  authDomain: "upper-a544e.firebaseapp.com",
+  projectId: "upper-a544e",
+  storageBucket: "upper-a544e.appspot.com",
+  messagingSenderId: "665713417470",
+  appId: "1:665713417470:web:73f7fb8ee518bea35999af",
+  measurementId: "G-QTFQ55YY5D",
+};
+
 function Register() {
+  const [auth, setAuth] = useState(null);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    const app = initializeApp(firebaseConfig);
+    const authInstance = getAuth(app);
+
+    if (authInstance && typeof authInstance === "object") {
+      setAuth(authInstance);
+    }
+
+    return () => {
+      // Lógica para limpiar Firebase si es necesario
+    };
+  }, []);
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      const response = await fetch("http://localhost:3001/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          firstName,
+          lastName,
+          email,
+          phoneNumber,
+        }),
+      });
+
+      console.log("Usuario registrado con éxito:", user);
+    } catch (error) {
+      console.error("Error al registrar el usuario:", error);
+    }
+  };
+
   return (
     <section className="">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -13,7 +78,7 @@ function Register() {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
               Create an account
             </h1>
-            <form className="space-y-4" action="#">
+            <form className="space-y-4" onSubmit={handleRegister}>
               <div className="mb-6 flex space-x-4">
                 <div className="relative" data-te-input-wrapper-init>
                   <input
@@ -52,44 +117,56 @@ function Register() {
                   />
                 </div>
               </div>
-            </form>
-            <div className="flex items-start flex-col">
-              <div className="mb-3 flex items-center h-5">
-                <input
-                  id="terms"
-                  type="checkbox"
-                  className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
-                  required=""
-                />
-                <label
-                  htmlFor="terms"
-                  className="ml-2 font-light text-gray-500"
-                >
-                  I accept the
-                  <a
-                    className="font-medium text-primary-600 hover:underline "
-                    href="#"
-                  >
-                    Terms and Conditions
-                  </a>
-                </label>
+              <div className="mb-6">
+                <div className="relative" data-te-input-wrapper-init>
+                  <input
+                    type="password"
+                    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none"
+                    id="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </div>
               </div>
-              <button
-                type="submit"
-                className="w-full bg-gray-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-              >
-                Create an account
-              </button>
-              <p className="mt-3 text-sm font-light text-gray-500">
-                Already have an account?
-                <a
-                  href="#"
-                  className="font-medium text-primary-600 hover:underline "
+              <div className="flex items-start flex-col">
+                <div className="mb-3 flex items-center h-5">
+                  <input
+                    id="terms"
+                    type="checkbox"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300"
+                    required=""
+                  />
+                  <label
+                    htmlFor="terms"
+                    className="ml-2 font-light text-gray-500"
+                  >
+                    I accept the
+                    <a
+                      className="font-medium text-primary-600 hover:underline "
+                      href="#"
+                    >
+                      Terms and Conditions
+                    </a>
+                  </label>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full bg-gray-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 >
-                  Login here
-                </a>
-              </p>
-            </div>
+                  Create an account
+                </button>
+                <p className="mt-3 text-sm font-light text-gray-500">
+                  Already have an account?
+                  <a
+                    href="#"
+                    className="font-medium text-primary-600 hover:underline "
+                  >
+                    Login here
+                  </a>
+                </p>
+              </div>
+            </form>
           </div>
         </div>
       </div>
