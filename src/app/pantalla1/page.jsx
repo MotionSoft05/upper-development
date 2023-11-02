82edf197c683024e73b50ab62539a3f331f7a46c
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-11-02 18:54:07 -0300

    Pantallas

---
+++ src/app/pantalla1/page.jsx
@@ -0,0 +1,5 @@
+function Pantalla1() {
+  return <h1>pantalla1</h1>;
+}
+
+export default Pantalla1;
---
+++ src/app/pantalla2/page.jsx
@@ -0,0 +1,5 @@
+function Pantalla2() {
+  return <h1>pantalla2</h1>;
+}
+
+export default Pantalla2;
