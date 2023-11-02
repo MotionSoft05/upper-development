9baf1ebc71832808e862e6ed85fa70dd714776b3
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-11-02 18:51:28 -0300

    Update SideBar.jsx

--- src/components/dashboard/SideBar.jsx
+++ src/components/dashboard/SideBar.jsx
@@ -129,43 +129,6 @@ function Sidebar({
               setShowAdmin(false);
               setShowAltaEvento(false);
               setShowConsultaEvento(false);
-              setShowPantallas(true);
-              setShowPantallaSalon(false);
-              setShowPantallaDirectorio(false);
-              setShowPublicidad(false);
-              setShowlicencia(false);
-              setShowGuia(false);
-              setShowSoporte(false);
-            }}
-          >
-            <a
-              href="#"
-              className="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
-            >
-              <span className="flex items-center justify-center text-lg text-gray-400">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="24"
-                  viewBox="0 -960 960 960"
-                  width="24"
-                >
-                  <path
-                    d="M320-120v-80H160q-33 0-56.5-23.5T80-280v-480q0-33 23.5-56.5T160-840h640q33 0 56.5 23.5T880-760v480q0 33-23.5 56.5T800-200H640v80H320ZM160-280h640v-480H160v480Zm0 0v-480 480Z"
-                    fill="#9ca3af"
-                  />
-                </svg>
-              </span>
-              <span className="ml-3">Pantallas</span>
-            </a>
-          </button>
-        </li>
-
-        <li className="my-px">
-          <button
-            onClick={() => {
-              setShowAdmin(false);
-              setShowAltaEvento(false);
-              setShowConsultaEvento(false);
               setShowPantallas(false);
               setShowPantallaSalon(true);
               setShowPantallaDirectorio(false);
@@ -265,6 +228,43 @@ function Sidebar({
                 </svg>
               </span>
               <span className="ml-3">Publicidad</span>
+            </a>
+          </button>
+        </li>
+
+        <li className="my-px">
+          <button
+            onClick={() => {
+              setShowAdmin(false);
+              setShowAltaEvento(false);
+              setShowConsultaEvento(false);
+              setShowPantallas(true);
+              setShowPantallaSalon(false);
+              setShowPantallaDirectorio(false);
+              setShowPublicidad(false);
+              setShowlicencia(false);
+              setShowGuia(false);
+              setShowSoporte(false);
+            }}
+          >
+            <a
+              href="#"
+              className="flex flex-row items-center h-10 px-3 rounded-lg text-gray-300 hover:bg-gray-100 hover:text-gray-700"
+            >
+              <span className="flex items-center justify-center text-lg text-gray-400">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24"
+                  viewBox="0 -960 960 960"
+                  width="24"
+                >
+                  <path
+                    d="M320-120v-80H160q-33 0-56.5-23.5T80-280v-480q0-33 23.5-56.5T160-840h640q33 0 56.5 23.5T880-760v480q0 33-23.5 56.5T800-200H640v80H320ZM160-280h640v-480H160v480Zm0 0v-480 480Z"
+                    fill="#9ca3af"
+                  />
+                </svg>
+              </span>
+              <span className="ml-3">Pantallas</span>
             </a>
           </button>
         </li>
