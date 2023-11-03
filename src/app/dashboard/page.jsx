667fa1bbc02c682265ac57242d368eacf71a37e8
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-11-03 04:56:26 -0300

    Update page.jsx

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -11,12 +11,39 @@ import Soporte from "@/components/dashboard/soporte";
 import Admin from "@/components/dashboard/admin";
 import Pantallas from "@/components/dashboard/pantallas";
 import Link from "next/link";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+// Configuración de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCzD--npY_6fZcXH-8CzBV7UGzPBqg85y8",
+  authDomain: "upper-a544e.firebaseapp.com",
+  projectId: "upper-a544e",
+  storageBucket: "upper-a544e.appspot.com",
+  messagingSenderId: "665713417470",
+  appId: "1:665713417470:web:73f7fb8ee518bea35999af",
+  measurementId: "G-QTFQ55YY5D",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
 function DashBoard() {
-  const [showAdmin, setShowAdmin] = useState(true);
+  const [userEmail, setUserEmail] = useState(null);
+  const [showAdmin, setShowAdmin] = useState(false);
+  const [showConsultaEvento, setShowConsultaEvento] = useState(true);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUserEmail(user ? user.email : null); // Actualiza el estado del correo electrónico del usuario
+      setShowConsultaEvento(true); // Muestra ConsultaModEvento por defecto
+    });
+
+    return () => unsubscribe();
+  }, []);
   const [showAltaEvento, setShowAltaEvento] = useState(false);
-  const [showConsultaEvento, setShowConsultaEvento] = useState(false);
 
   const [showPantallas, setShowPantallas] = useState(false);
   const [showPantallaSalon, setShowPantallaSalon] = useState(false);
@@ -32,6 +59,7 @@ function DashBoard() {
     <div className="flex flex-row min-h-screen  ">
       <aside className="sidebar w-64 md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in bg-indigo-500">
         <Sidebar
+          userEmail={userEmail}
           setShowAdmin={setShowAdmin}
           setShowAltaEvento={setShowAltaEvento}
           setShowConsultaEvento={setShowConsultaEvento}
@@ -46,7 +74,7 @@ function DashBoard() {
       </aside>
       <main className="main flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in">
         <div className="main-content flex flex-col flex-grow p-4">
-          {showAdmin && <Admin />}
+          {showAdmin && userEmail === "uppermex10@gmail.com" && <Admin />}
           {showAltaEvento && <AltaEventos />}
           {showConsultaEvento && <ConsultaModEvento />}
 
