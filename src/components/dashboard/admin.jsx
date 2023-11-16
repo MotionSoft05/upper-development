b365826f19e39567517432124825559ccccf3f39
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-11-15 21:16:27 -0300

    Update admin.jsx

--- src/components/dashboard/admin.jsx
+++ src/components/dashboard/admin.jsx
@@ -40,14 +40,19 @@ function Admin() {
     nombre: "",
     fecha: "",
     monto: "",
-    plan: "",
+    ps: "", // Cambiado de "plan" a "ps"
+    pd: "", // Nuevo campo "pd"
+    total: "", // Nuevo campo "numero"
   });
+
   const [transaccionEditada, setTransaccionEditada] = useState({
     id: "",
     nombre: "",
     fecha: "",
     monto: "",
-    plan: "",
+    ps: "", // Cambiado de "plan" a "ps"
+    pd: "", // Nuevo campo "pd"
+    total: "", // Nuevo campo "numero"
   });
   const [transacciones, setTransacciones] = useState([]);
   const [currentUser, setCurrentUser] = useState(null);
@@ -76,7 +81,9 @@ function Admin() {
         !nuevaTransaccion.nombre ||
         !nuevaTransaccion.fecha ||
         !nuevaTransaccion.monto ||
-        !nuevaTransaccion.plan
+        !nuevaTransaccion.ps ||
+        !nuevaTransaccion.pd ||
+        !nuevaTransaccion.total
       ) {
         alert("Por favor, completa todos los campos de la transacción.");
         return;
@@ -86,15 +93,19 @@ function Admin() {
         collection(db, "transacciones"),
         nuevaTransaccion
       );
+
       setTransacciones([
         ...transacciones,
         { id: transaccionRef.id, ...nuevaTransaccion },
       ]);
+
       setNuevaTransaccion({
         nombre: "",
         fecha: "",
         monto: "",
-        plan: "",
+        ps: "",
+        pd: "",
+        total: "",
       });
     } catch (error) {
       console.error("Error al guardar la transacción en Firebase:", error);
@@ -149,11 +160,19 @@ function Admin() {
   const handleGuardarCambios = async () => {
     try {
       const usuarioDocRef = doc(db, "usuarios", usuarioEditado.id);
+
+      // Convierte los valores de cadena a números antes de la actualización
+      const psNumber = parseInt(usuarioEditado.ps);
+      const pdNumber = parseInt(usuarioEditado.pd);
+
       await updateDoc(usuarioDocRef, {
         nombre: usuarioEditado.nombre,
         apellido: usuarioEditado.apellido,
         email: usuarioEditado.email,
         telefono: usuarioEditado.telefono,
+        ps: psNumber,
+        pd: pdNumber,
+        total: psNumber + pdNumber, // Suma correctamente los valores
       });
 
       setUsuarios((prevUsuarios) =>
@@ -165,6 +184,9 @@ function Admin() {
                 apellido: usuarioEditado.apellido,
                 email: usuarioEditado.email,
                 telefono: usuarioEditado.telefono,
+                ps: psNumber,
+                pd: pdNumber,
+                total: psNumber + pdNumber,
               }
             : usuario
         )
@@ -177,6 +199,9 @@ function Admin() {
         apellido: "",
         email: "",
         telefono: "",
+        ps: "",
+        pd: "",
+        total: "",
       });
     } catch (error) {
       console.error("Error al guardar los cambios en Firebase:", error);
@@ -194,20 +219,21 @@ function Admin() {
     }
   };
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-    });
     const obtenerUsuarios = async () => {
-      const usuariosCollection = collection(db, "usuarios");
-      const usuariosSnapshot = await getDocs(usuariosCollection);
-      const usuariosData = usuariosSnapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setUsuarios(usuariosData);
+      try {
+        const usuariosCollection = collection(db, "usuarios");
+        const usuariosSnapshot = await getDocs(usuariosCollection);
+        const usuariosData = usuariosSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setUsuarios(usuariosData);
+      } catch (error) {
+        console.error("Error al obtener los usuarios de Firebase:", error);
+      }
     };
+
     obtenerUsuarios();
-    return () => unsubscribe();
   }, []);
   if (currentUser && currentUser.email !== "uppermex10@gmail.com") {
     // Si el usuario no tiene permitido el acceso, puedes redirigirlo a otra página
@@ -248,11 +274,18 @@ function Admin() {
                   <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">
                     Teléfono
                   </th>
+
                   <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">
-                    Acciones
+                    PS
                   </th>
                   <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">
-                    Plan
+                    PD
+                  </th>
+                  <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">
+                    Total
+                  </th>
+                  <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">
+                    Acciones
                   </th>
                 </tr>
               </thead>
@@ -312,6 +345,61 @@ function Admin() {
                     </td>
                     <td className="py-2 px-4 border-b border-grey-light">
                       {modoEdicion && usuarioEditado.id === usuario.id ? (
+                        <input
+                          type="text"
+                          value={usuarioEditado.ps}
+                          onChange={(e) =>
+                            setUsuarioEditado({
+                              ...usuarioEditado,
+                              ps: e.target.value,
+                              total:
+                                parseInt(e.target.value) +
+                                parseInt(usuarioEditado.pd),
+                            })
+                          }
+                        />
+                      ) : (
+                        usuario.ps
+                      )}
+                    </td>
+                    <td className="py-2 px-4 border-b border-grey-light">
+                      {modoEdicion && usuarioEditado.id === usuario.id ? (
+                        <input
+                          type="text"
+                          value={usuarioEditado.pd}
+                          onChange={(e) =>
+                            setUsuarioEditado({
+                              ...usuarioEditado,
+                              pd: e.target.value,
+                              total:
+                                parseInt(usuarioEditado.ps) +
+                                parseInt(e.target.value),
+                            })
+                          }
+                        />
+                      ) : (
+                        usuario.pd
+                      )}
+                    </td>
+                    <td className="py-2 px-4 border-b border-grey-light">
+                      {modoEdicion && usuarioEditado.id === usuario.id ? (
+                        <input
+                          type="text"
+                          value={usuarioEditado.total}
+                          onChange={(e) =>
+                            setUsuarioEditado({
+                              ...usuarioEditado,
+                              total: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        usuario.total
+                      )}
+                    </td>
+
+                    <td className="py-2 px-4 border-b border-grey-light">
+                      {modoEdicion && usuarioEditado.id === usuario.id ? (
                         <div className="flex space-x-2">
                           <button
                             onClick={handleGuardarCambios}
@@ -347,213 +435,6 @@ function Admin() {
                 ))}
               </tbody>
             </table>
-          </div>
-
-          <div className="mt-8 bg-white p-4 shadow rounded-lg">
-            <div className="bg-white p-4 rounded-md mt-4">
-              <h2 className="text-gray-500 text-lg font-semibold pb-4">
-                Transacciones
-              </h2>
-              <div className="mb-6 border-b border-gray-300"></div>
-              <div className="flex items-center mb-4 space-x-2">
-                <input
-                  className="p-2 rounded border border-gray-300"
-                  type="text"
-                  value={nuevaTransaccion.nombre}
-                  onChange={(e) =>
-                    setNuevaTransaccion({
-                      ...nuevaTransaccion,
-                      nombre: e.target.value,
-                    })
-                  }
-                  placeholder="Nombre y Apellido"
-                />
-                <input
-                  className="p-2 rounded border border-gray-300"
-                  type="text"
-                  value={nuevaTransaccion.fecha}
-                  onChange={(e) =>
-                    setNuevaTransaccion({
-                      ...nuevaTransaccion,
-                      fecha: e.target.value,
-                    })
-                  }
-                  placeholder="Fecha"
-                />
-                <input
-                  className="p-2 rounded border border-gray-300"
-                  type="text"
-                  value={nuevaTransaccion.monto}
-                  onChange={(e) =>
-                    setNuevaTransaccion({
-                      ...nuevaTransaccion,
-                      monto: e.target.value,
-                    })
-                  }
-                  placeholder="Monto"
-                />
-                <input
-                  className="p-2 rounded border border-gray-300"
-                  type="text"
-                  value={nuevaTransaccion.plan}
-                  onChange={(e) =>
-                    setNuevaTransaccion({
-                      ...nuevaTransaccion,
-                      plan: e.target.value,
-                    })
-                  }
-                  placeholder="Plan"
-                />
-                <button
-                  onClick={handleGuardarTransaccion}
-                  className="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded"
-                >
-                  Guardar
-                </button>
-              </div>
-              <table className="w-full table-auto text-sm">
-                <thead>
-                  <tr className="text-sm leading-normal">
-                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">
-                      Nombre y Apellido
-                    </th>
-                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">
-                      Fecha
-                    </th>
-                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">
-                      Monto
-                    </th>
-                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">
-                      Plan
-                    </th>
-                    <th className="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-light border-b border-grey-light text-left">
-                      Acciones
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {transacciones.map((transaccion) => (
-                    <tr className="hover:bg-grey-lighter" key={transaccion.id}>
-                      <td className="py-2 px-4 border-b border-grey-light">
-                        {modoEdiciontransaccion &&
-                        transaccionEditada.id === transaccion.id ? (
-                          <input
-                            type="text"
-                            value={transaccionEditada.nombre}
-                            onChange={(e) =>
-                              setTransaccionEditada({
-                                ...transaccionEditada,
-                                nombre: e.target.value,
-                              })
-                            }
-                          />
-                        ) : (
-                          transaccion.nombre
-                        )}
-                      </td>
-                      <td className="py-2 px-4 border-b border-grey-light">
-                        {modoEdiciontransaccion &&
-                        transaccionEditada.id === transaccion.id ? (
-                          <input
-                            type="text"
-                            value={transaccionEditada.fecha}
-                            onChange={(e) =>
-                              setTransaccionEditada({
-                                ...transaccionEditada,
-                                fecha: e.target.value,
-                              })
-                            }
-                          />
-                        ) : (
-                          transaccion.fecha
-                        )}
-                      </td>
-                      <td className="py-2 px-4 border-b border-grey-light">
-                        {modoEdiciontransaccion &&
-                        transaccionEditada.id === transaccion.id ? (
-                          <input
-                            type="text"
-                            value={transaccionEditada.monto}
-                            onChange={(e) =>
-                              setTransaccionEditada({
-                                ...transaccionEditada,
-                                monto: e.target.value,
-                              })
-                            }
-                          />
-                        ) : (
-                          transaccion.monto
-                        )}
-                      </td>
-                      <td className="py-2 px-4 border-b border-grey-light">
-                        {modoEdiciontransaccion &&
-                        transaccionEditada.id === transaccion.id ? (
-                          <input
-                            type="text"
-                            value={transaccionEditada.plan}
-                            onChange={(e) =>
-                              setTransaccionEditada({
-                                ...transaccionEditada,
-                                plan: e.target.value,
-                              })
-                            }
-                          />
-                        ) : (
-                          transaccion.plan
-                        )}
-                      </td>
-                      <td className="py-2 px-4 border-b border-grey-light">
-                        {modoEdiciontransaccion &&
-                        transaccionEditada.id === transaccion.id ? (
-                          <div className="flex space-x-2">
-                            <button
-                              onClick={handleGuardarCambiosTransaccion}
-                              className="bg-green-500 hover:bg-green-700 text-white font-semibold py-1 px-2 rounded"
-                            >
-                              Guardar
-                            </button>
-                            <button
-                              onClick={() => {
-                                setModoEdiciontransaccion(false);
-                                setTransaccionEditada({
-                                  id: "",
-                                  nombre: "",
-                                  fecha: "",
-                                  monto: "",
-                                  plan: "",
-                                });
-                              }}
-                              className="bg-gray-500 hover:bg-gray-700 text-white font-semibold py-1 px-2 rounded"
-                            >
-                              Cancelar
-                            </button>
-                          </div>
-                        ) : (
-                          <div className="flex space-x-2">
-                            <button
-                              onClick={() =>
-                                handleEditarTransaccion(transaccion)
-                              }
-                              className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-1 px-2 rounded"
-                            >
-                              Editar
-                            </button>
-                            <button
-                              onClick={() =>
-                                handleEliminarTransaccion(transaccion.id)
-                              }
-                              className="bg-red-500 hover:bg-red-700 text-white font-semibold py-1 px-2 rounded"
-                            >
-                              Eliminar
-                            </button>
-                          </div>
-                        )}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
           </div>
         </div>
       </div>
