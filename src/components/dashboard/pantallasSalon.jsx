2c29ffc1ce1120c5edf85e30a8991e854ed97ba7
MotionSoft05 <kevinbbarrios05@gmail.com>
2023-12-01 17:46:20 -0300

    Update pantallasSalon.jsx

--- src/components/dashboard/pantallasSalon.jsx
+++ src/components/dashboard/pantallasSalon.jsx
@@ -486,6 +486,7 @@ function PantallasSalon() {
                     />
                     <Link
                       href={`/pantalla${index + 1}`}
+                      target="_blank"
                       className="bg-gray-300 hover:bg-gray-500 text-white font-bold py-2 px-4  active:bg-gray-500"
                     >
                       URL
