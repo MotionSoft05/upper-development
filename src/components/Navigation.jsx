4ff45e0ff5ffca4ac34d922c039ee5615c83e969
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-12-26 13:56:38 -0300

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -37,7 +37,6 @@ const usuariosCollection = collection(db, "usuarios");
 const Loader = () => (
   <div className="h-screen bg-white">
     <div className="flex justify-center items-center h-full">
-   
       <img
         className="h-16 w-16"
         src="https://icons8.com/preloaders/preloaders/1488/Iphone-spinner-2.gif"
