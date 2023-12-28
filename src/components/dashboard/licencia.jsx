00f29f0889c31abdd94fd98e4ad935e294c5c40a
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2023-12-28 01:17:21 -0300

    Update licencia.jsx

--- src/components/dashboard/licencia.jsx
+++ src/components/dashboard/licencia.jsx
@@ -306,7 +306,7 @@ function DatosFiscales({ currentUser }) {
             id="email"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
             placeholder="ejemplo@gmail.com"
-            defaultValue={currentUser ? currentUser.email : ""}
+            defaultValue={datosFiscales.email}
             required
           />
         </div>
