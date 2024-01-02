569105c78f0abc6e5b3ccf1c266668af74c71f65
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2024-01-02 17:33:27 -0300

    Update PantallasDirectorio.jsx

--- src/components/dashboard/PantallasDirectorio.jsx
+++ src/components/dashboard/PantallasDirectorio.jsx
@@ -130,6 +130,10 @@ function PantallasDirectorio() {
     fetchTemplateData();
   }, []);
 
+  const handleCityChange = (selectedOption) => {
+    setSelectedCity(selectedOption);
+  };
+
   const fontStyleOptions = [
     { value: "Arial", label: "Arial" },
     { value: "Times New Roman", label: "Times New Roman" },
@@ -249,7 +253,10 @@ function PantallasDirectorio() {
         return;
       }
 
-      // console.log("Usuario autenticado:", authUser);
+      if (!selectedCity) {
+        alert("Por favor, seleccione una ciudad.");
+        return;
+      }
 
       if (!selectedLogo) {
         console.error("selectedLogo es null. No se puede enviar a Firestore.");
@@ -261,6 +268,7 @@ function PantallasDirectorio() {
         templateColor: templateColor,
         fontStyle: selectedFontStyle.value,
         logo: selectedLogo,
+        ciudad: selectedCity.value,
       };
 
       const templateDirectoriosRef = collection(db, "TemplateDirectorios");
@@ -280,6 +288,7 @@ function PantallasDirectorio() {
           templateColor: templateColor,
           fontStyle: selectedFontStyle.value,
           logo: selectedLogo,
+          ciudad: selectedCity.value,
           timestamp: serverTimestamp(),
         });
       } else {
@@ -289,6 +298,7 @@ function PantallasDirectorio() {
           templateColor: templateColor,
           fontStyle: selectedFontStyle.value,
           logo: selectedLogo,
+          ciudad: selectedCity.value,
           timestamp: serverTimestamp(),
         });
       }
@@ -438,9 +448,12 @@ function PantallasDirectorio() {
               <Select
                 options={cityOptions}
                 value={selectedCity}
-                onChange={setSelectedCity}
+                onChange={handleCityChange}
                 placeholder="Seleccione una ciudad"
                 className="w-full"
+                isSearchable
+                isClearable={false}
+                required // Asegura que la ciudad sea obligatoria
               />
             </div>
 
@@ -504,182 +517,6 @@ function PantallasDirectorio() {
                 </div>
               ))}
             </div>
-          </div>
-          ;{/* Sección de vista previa */}
-          {previewVisible && (
-            <div className="fixed top-0 left-0 flex items-center justify-center w-screen h-screen bg-black bg-opacity-80 z-50">
-              <div className="bg-white w-2/4  p-6 rounded-md shadow-lg text-black  ">
-                <div className="flex justify-between items-center">
-                  {/* Logo en la esquina superior izquierda */}
-                  <div className="">
-                    {logo ? (
-                      // eslint-disable-next-line @next/next/no-img-element
-                      <img
-                        src={URL.createObjectURL(logo)}
-                        alt="Logo"
-                        className="h-28 mb-2"
-                      />
-                    ) : (
-                      // eslint-disable-next-line @next/next/no-img-element
-                      <img
-                        src="/img/fiestamericana.png"
-                        alt="Logo"
-                        className="h-15"
-                      />
-                    )}
-                  </div>
-
-                  <div
-                    className="flex flex-col items-center"
-                    style={{
-                      color: fontColor,
-                      fontFamily: selectedFontStyle
-                        ? selectedFontStyle.value
-                        : "Arial",
-                    }}
-                  >
-                    <p className="text-2xl text-center font-semibold mb-2">
-                      {`${obtenerDia()} ${obtenerFecha()} - ${currentTime}`}
-                    </p>
-                    <h1 className="text-4xl font-bold">Eventos del día</h1>
-                  </div>
-
-                  <div className="flex flex-col" style={{ color: fontColor }}>
-                    {isLoading ? (
-                      <p>Cargando datos del clima...</p>
-                    ) : weatherData &&
-                      weatherData.current &&
-                      weatherData.current.temp_c ? (
-                      <p className="text-3xl font-bold">
-                        {weatherData.current.temp_c} °C
-                      </p>
-                    ) : (
-                      <p>No se pudo obtener la información del clima</p>
-                    )}
-                  </div>
-                </div>
-                <div className="bg-gradient-to-t from-gray-50  to-white text-gray-50">
-                  <div className="">
-                    <div
-                      className={` text-white text-2xl font-semibold mt-1 text-center justify-between flex px-20 mb-4 rounded-t-xl`}
-                      style={{
-                        color: fontColor,
-                        backgroundColor: templateColor,
-                        fontFamily: selectedFontStyle
-                          ? selectedFontStyle.value
-                          : "Arial",
-                      }}
-                    >
-                      {/* Título */}
-                      <h2 className=" text-white"> </h2>
-                    </div>
-                    <div className=" text-black">
-                      {/* Imagen a la izquierda */}
-                      <div
-                        className="flex flex-col
-                      "
-                      >
-                        <div className="flex items-center border-b border-black w-full">
-                          <div className="space-y-5 pl-5 flex-grow">
-                            {selectedEvents &&
-                              selectedEvents.map((event) => {
-                                return (
-                                  <div
-                                    key={event.id}
-                                    className="flex items-center space-x-4"
-                                  >
-                                    {/* Imagen a la izquierda */}
-                                    <img
-                                      src={event.images[0]}
-                                      alt={event.nombreEvento}
-                                      style={{
-                                        width: "130px",
-                                        height: "110px",
-                                      }}
-                                    />
-
-                                    {/* Detalles del evento */}
-                                    <div
-                                      style={{
-                                        color: fontColor,
-                                        fontFamily: selectedFontStyle
-                                          ? selectedFontStyle.value
-                                          : "Arial",
-                                      }}
-                                    >
-                                      {/* Aplicando el color seleccionado */}
-                                      <h3>{event.nombreEvento}</h3>
-                                      <p>{event.tipoEvento}</p>
-                                      <p>{event.lugar}</p>
-                                      {/* Agrega más detalles según sea necesario */}
-                                      <div className="text-right">
-                                        <p>{event.horaInicialReal}</p>
-                                      </div>
-                                    </div>
-                                  </div>
-                                );
-                              })}
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div>
-                      {/* Fecha y hora en la esquina inferior */}
-
-                      <div
-                        className={`text-2xl font-semibold mt-1 text-center text-white bg-black justify-between flex px-20 rounded-b-xl`}
-                        style={{
-                          color: fontColor,
-                          backgroundColor: templateColor,
-                          fontFamily: selectedFontStyle
-                            ? selectedFontStyle.value
-                            : "Arial",
-                        }}
-                      >
-                        <p> </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center">
-                  <p
-                    className=""
-                    style={{
-                      color: fontColor,
-                      fontFamily: selectedFontStyle
-                        ? selectedFontStyle.value
-                        : "Arial",
-                    }}
-                  >
-                    Grupo renueca el mejor programa de recompensa para
-                    asistentes ejec
-                  </p>
-                  <img
-                    src="/img/licensed-image.jpeg"
-                    alt="Logo"
-                    className="h-12"
-                  />
-                </div>
-                {/* Botón para volver atrás */}
-                <button
-                  onClick={handleClosePreview}
-                  className="absolute top-4 right-4 bg-gray-300 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded-full"
-                >
-                  Volver atrás
-                </button>
-              </div>
-            </div>
-          )}
-          <div className="flex justify-end mt-6">
-            {/*
-            <button
-              onClick={handlePreviewClick}
-              className="mx-5 px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600"
-            >
-              Vista Previa
-            </button>
-            */}
             <button
               onClick={() => {
                 guardarInformacionPersonalizacion(selectedLogo);
