c171fc12365d7fb1a196556e8015bb4e890dfb06
MotionSoft05 <kevinbbarrios05@gmail.com>
2024-01-15 17:29:31 -0300

    Create page.jsx

---
+++ src/app/paginasAleatorias/page.jsx
@@ -0,0 +1,11 @@
+function PaginaLista() {
+  return (
+    <>
+      <h1>Lista paginas</h1>
+      <h2>Pagina 1</h2>
+      <h2>Pagina 2</h2>
+      <h2>Pagina 3</h2>
+    </>
+  );
+}
+export default PaginaLista;
