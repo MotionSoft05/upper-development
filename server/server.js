38c8277739329c4556bf4363390889eda7e61418
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2024-01-17 15:32:34 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const cors = require("cors");
-
+const auth = require("./firebaseAdmin");
 const parser = require("fast-xml-parser");
 
 const allowedOrigins = ["http://localhost:3000", "https://upperds.mx/"];
@@ -52,6 +52,25 @@ app.get("/fetch-rss", async (req, res) => {
   } catch (error) {
     console.error("Error fetching or parsing data:", error);
     res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.delete("/eliminar-usuario/:uid", async (req, res) => {
+  const uid = req.params.uid;
+
+  res.header("Access-Control-Allow-Origin", "*");
+
+  try {
+    await auth.deleteUser(uid);
+    res
+      .status(200)
+      .json({ mensaje: `Usuario con UID ${uid} eliminado correctamente.` });
+  } catch (error) {
+    console.error(
+      "Error al eliminar usuario de Firebase Authentication:",
+      error
+    );
+    res.status(500).json({ error: "Error interno del servidor." });
   }
 });
 
