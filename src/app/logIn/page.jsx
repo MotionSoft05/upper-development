4fa4d1e1ac840439d1415db486e2c28133cec00e
MotionSoft05 <kevinbbarrios05@gmail.com>
2024-02-01 11:00:25 -0300

    Tiempo publicidad, Bucle video publicidad, estilo direc arreglado

--- src/app/logIn/page.jsx
+++ src/app/logIn/page.jsx
@@ -194,9 +194,9 @@ function LogIn() {
                 <button
                   type="submit"
                   disabled={!isFormValid}
-                  className={`inline-block rounded bg-green-300 text-white px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out ${
+                  className={`inline-block rounded bg-gray-400 text-white px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out ${
                     isFormValid
-                      ? "hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+                      ? "bg-green-300 hover:bg-primary hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
                       : "cursor-not-allowed bg-gray-400 text-gray-200"
                   }`}
                 >
