64b4b7fda1fea8f82c8a20587c81b23d678eb3c8
pablopissoni <pablopissoni@gmail.com>
2024-04-30 20:54:20 -0300

    traduccion completa del componente publicidad.jsx

--- src/components/dashboard/publicidad.jsx
+++ src/components/dashboard/publicidad.jsx
@@ -2,8 +2,10 @@
 import React, { useState } from "react";
 import PublicidadSalon from "./publicidadSalon";
 import PublicidadDirect from "./publicidadDirec";
+import { useTranslation } from "react-i18next";
 
 function Publicidad() {
+  const {t} = useTranslation()
   const [showSalon, setShowSalon] = useState(true);
 
   return (
@@ -12,15 +14,16 @@ function Publicidad() {
         <section className="">
           <div className="mb-8">
             <h2 className="text-3xl font-extrabold text-gray-900">
-              CONFIGURACIÓN DE PUBLICIDAD
+              {/* CONFIGURACIÓN DE PUBLICIDAD */}
+              {t('advertisement.title')}
             </h2>
             <p className="text-gray-600">
-              Ingresar imágenes que serán desplegadas cuando no se cuente con
-              eventos.
+              {/* Ingresar imágenes que serán ... */}
+              {t('advertisement.description1')}
             </p>
             <p className="text-gray-600">
-              El tamaño recomendado es el de tu pantalla. Cómo máximo 10
-              publicidades por sección.
+              {/* El tamaño recomendado es el ... */}
+              {t('advertisement.description2')}
             </p>
           </div>
 
@@ -33,7 +36,8 @@ function Publicidad() {
                   : "bg-gray-400 text-gray-700"
               } px-4 py-2 rounded-md focus:outline-none`}
             >
-              Salón de Eventos
+              {/* Salón de Eventos */}
+              {t('advertisement.eventRoom')}
             </button>
             <button
               onClick={() => setShowSalon(false)}
@@ -43,7 +47,8 @@ function Publicidad() {
                   : "bg-gray-400 text-gray-700"
               } ml-4 px-4 py-2 rounded-md focus:outline-none`}
             >
-              Directorio de Eventos
+              {/* Directorio de Eventos */}
+              {t('advertisement.eventDirectory')}
             </button>
           </div>
 
