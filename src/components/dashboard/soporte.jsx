d86a2f6a191279e22fd47bac6992ccaff2f68c3e
pablopissoni <pablopissoni@gmail.com>
2024-05-02 14:51:52 -0300

    Traduccion completa soporte.jsx

--- src/components/dashboard/soporte.jsx
+++ src/components/dashboard/soporte.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
+import { useTranslation } from "react-i18next";
 
 function Soporte() {
+
+  const {t} = useTranslation()
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
@@ -14,14 +17,16 @@ function Soporte() {
     // Validar campos
     if (!name || !email || !message) {
       setError(
-        "Por favor, completa todos los campos antes de enviar el mensaje."
+        // "Por favor, completa todos los campos antes de enviar el mensaje."
+        t("support.errorMessageFields")
       );
       return;
     }
 
     // Validar correo electrónico
     if (!emailRegex.test(email)) {
-      setError("Por favor, ingresa un correo electrónico válido.");
+      // "Por favor, ingresa un correo electrónico válido."
+      setError(t("support.validEmailError"));
       return;
     }
 
@@ -38,14 +43,17 @@ function Soporte() {
 
     emailjs.send(serviceId, templateId, templateParams, userId).then(
       (response) => {
-        console.log("Correo electrónico enviado:", response);
+        // "Correo electrónico enviado:"
+        console.log(t("support.consoleSuccess"), response);
         setSuccess(true);
         setError("");
       },
       (error) => {
-        console.error("Error al enviar el correo electrónico:", error);
+        // "Error al enviar el correo electrónico:"
+        console.error(t("support.consoleError"), error);
         setError(
-          "Hubo un error al enviar el mensaje. Por favor, inténtalo de nuevo más tarde."
+          // "Hubo un error al enviar el mensaje. Por favor, inténtalo de nuevo más tarde."
+          t("support.errorMessage")
         );
       }
     );
@@ -55,7 +63,8 @@ function Soporte() {
     <section className="px-5 md:px-32">
       <div className="p-5">
         <h1 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl">
-          CONTACTO SOPORTE
+          {/* CONTACTO SOPORTE */}
+          {t("support.contactTitle")}
         </h1>
       </div>
       <div className="container px-6 md:px-12 mt-10">
@@ -63,18 +72,16 @@ function Soporte() {
           <div className="mb-12 ">
             <div className="flex flex-col space-y-5 mx-auto mb-12 text-center lg:mb-0">
               <h1 className="font-medium">
-                ¿Estás experimentando algún problema técnico que necesitas
-                resolver? ¡No te preocupes! Estamos aquí para ayudarte.
+                {/* ¿Estás experimentando algún ... */}
+                {t("support.contactSubtitle1")}
               </h1>
               <h2 className="font-medium">
-                Para agilizar el proceso y brindarte la asistencia que
-                necesitas, te invitamos a generar un reporte para nuestro equipo
-                de soporte técnico. ¡Es fácil y rápido!
+                {/* Para agilizar el proceso y brindarte ... */}
+                {t("support.contactSubtitle2")}
               </h2>
               <h2 className="font-medium">
-                Simplemente completa el formulario con todos los detalles
-                relevantes sobre el problema que estás enfrentando. Cuanta más
-                información nos proporciones, mejor podremos ayudarte.
+                {/* Simplemente completa el formulario con todos ... */}
+                {t("support.contactSubtitle3")}
               </h2>
             </div>
           </div>
@@ -83,9 +90,9 @@ function Soporte() {
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <input
                   type="text"
-                  className="w-full rounded bg-transparent py-[0.32rem] text-black border border-sky-100 "
+                  className="w-full rounded bg-transparent pl-2 py-[0.32rem] text-black border border-sky-100 "
                   id="exampleInput90"
-                  placeholder="Nombre"
+                  placeholder={t("support.namePlaceholder")}
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
@@ -93,18 +100,18 @@ function Soporte() {
               <div className="relative mb-6" data-te-input-wrapper-init>
                 <input
                   type="email"
-                  className="w-full rounded bg-transparent py-[0.32rem] text-black border border-sky-100 "
-                  placeholder="Correo electrónico"
+                  className="w-full rounded bg-transparent pl-2 py-[0.32rem] text-black border border-sky-100 "
+                  placeholder={t("support.emailPlaceholder")}
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
               <div>
                 <textarea
-                  className="w-full rounded bg-transparent py-[0.32rem] text-black border border-sky-100 "
+                  className="w-full rounded bg-transparent pl-2 py-[0.32rem] text-black border border-sky-100 "
                   id="exampleFormControlTextarea1"
                   rows="3"
-                  placeholder="Su mensaje"
+                  placeholder={t("support.messagePlaceholder")}
                   value={message}
                   onChange={(e) => setMessage(e.target.value)}
                 ></textarea>
@@ -115,14 +122,15 @@ function Soporte() {
                 onClick={sendEmail}
                 className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
               >
-                Enviar
+                {/* Enviar */}
+                {t("support.sendButton")}
               </button>
             </form>
             {error && <p className="text-red-500 text-sm mt-2">{error}</p>}
             {success && (
               <p className="text-green-500 text-sm mt-2">
-                ¡Mensaje enviado con éxito! Nos pondremos en contacto contigo
-                pronto.
+                {/* ¡Mensaje enviado con éxito! Nos pondremos en ... */}
+                {t("support.successMessage")}
               </p>
             )}
           </div>
