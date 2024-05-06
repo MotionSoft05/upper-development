d87eabf17a1440384014290b6c1397e97468be4a
pablopissoni <pablopissoni@gmail.com>
2024-05-06 19:00:12 -0300

    fix className

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -533,26 +533,26 @@ function Register() {
                   </button>
                 </div>
               </div>
-              <div class="flex items-start mb-5">
-                <div class="flex items-center h-5">
+              <div className="flex items-start mb-5">
+                <div className="flex items-center h-5">
                   <input
                     id="terms"
                     type="checkbox"
                     value=""
-                    class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
                     required
                     onChange={(e) => setTermsChecked(e.target.checked)}
                   />
                 </div>
                 <label
                   htmlFor="terms"
-                  class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                  className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                 >
                   {/* Estoy de acuerdo con los */}
                   {t("register.termsAndConditions")}
                   <a
                     href="#"
-                    class="pl-1 text-blue-600 hover:underline dark:text-blue-500"
+                    className="pl-1 text-blue-600 hover:underline dark:text-blue-500"
                   >
                     {/* términos y condiciones */}
                     {t("register.termsLink")}
