f71c54e004273307730f25a774d17874f150db2e
pablopissoni <pablopissoni@gmail.com>
2024-05-23 13:04:57 -0300

    Update page.jsx

--- src/app/pantallaDeServicio/page.jsx
+++ src/app/pantallaDeServicio/page.jsx
@@ -1,6 +1,6 @@
 function PantallaServicio() {
   return (
-    <div className="min-h-screen bg-red-100 grid grid-rows-6">
+    <div className="min-h-screen bg-red-100 grid grid-rows-6 gap-1">
 
       {/* SECCION 1, 2, 3 */}
       <div className="grid grid-cols-3 gap-1 mb-1 h-full row-span-5">
@@ -8,29 +8,29 @@ function PantallaServicio() {
         {/* Seccion superior izquierda */}
         <div className="grid grid-rows-2 gap-1 ">
           <div className="bg-cyan-200 border-2 border-slate-300 p-1">
-            seccion 1
+            IMAGEN
           </div>
           <div className="bg-cyan-200 border-2 border-slate-300 p-1">
-            seccion 2
+            IMAGEN
           </div>
         </div>
 
         {/* Seccion superior derecha */}
         <div className="bg-green-100 col-span-2 border-2 border-slate-300 p-1">
-          seccion 3
+          IMAGEN O VIDEO
         </div>
       </div>
 
       {/* SECCION FECHA, RSS, CLIMA */}
       <div className="grid grid-cols-3 gap-1 h-full flex-grow row-span-1">
         <div className="bg-orange-200 border-2 border-slate-300 p-1">
-          section
+          FECHA
         </div>
         <div className="bg-orange-200 border-2 border-slate-300 p-1">
-          section
+          RSS
         </div>
         <div className="bg-orange-200 border-2 border-slate-300 p-1">
-          section
+          CLIMA
         </div>
       </div>
     </div>
