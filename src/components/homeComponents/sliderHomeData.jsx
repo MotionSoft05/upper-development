3bd3459217fb3ec882c3cddae79b76f994f27808
Emanuel Nicolas Alessi <emanuelalessi14@gmail.com>
2024-05-28 23:32:53 -0300

    cambios en home

--- src/components/homeComponents/sliderHomeData.jsx
+++ src/components/homeComponents/sliderHomeData.jsx
@@ -42,14 +42,14 @@ export const SliderHomeData = ({ slider, index, t }) => {
         <div className="max-w-[850px]">
           {/*------------------------ HERO CONTENT START ------------------------*/}
           <div className="flex flex-col gap-6 ">
-            <p className="px-5 py-2 border-solid border-3 border-primary rounded-full w-fit md:text-2xl text-xl">
-            {t(`homeSliderData.sliderTag${index + 1}`)}
-            </p>
+            <p className="px-5 py-2 border-solid border-3 border-primary rounded-full w-fit md:text-2xl text-xl"></p>
 
             <h1 className="lg:text-6xl text-4xl font-bold lg:leading-[70px] flex gap-3">
-            {t(`homeSliderData.sliderTitle${index + 1}`)}
+              {t(`homeSliderData.sliderTitle${index + 1}`)}
             </h1>
-            <p className="lg:text-lg mb-6">{t(`homeSliderData.sliderDescription${index + 1}`)}</p>
+            <p className="lg:text-lg mb-6">
+              {t(`homeSliderData.sliderDescription${index + 1}`)}
+            </p>
           </div>
 
           {/* BUTTONS */}
