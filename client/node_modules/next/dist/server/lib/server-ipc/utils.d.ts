0851c11b1a27f4d56fa3b0e27c418dee2a650fa5
pablopissoni <pablopissoni@gmail.com>
2024-06-25 11:54:31 -0300

    se quito node_modules

--- client/node_modules/next/dist/server/lib/server-ipc/utils.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const actionsForbiddenHeaders: string[];
-export declare const filterReqHeaders: (headers: Record<string, undefined | string | number | string[]>, forbiddenHeaders: string[]) => Record<string, string | string[] | undefined>;
