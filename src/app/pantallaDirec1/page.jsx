8b74afa1cdef394d471f15bbd75ef6672d352ad2
MotionSoft05 <kevinbbarrios05@gmail.com>
2025-03-18 19:56:41 -0300

    Borrado las pantallas viejas

--- src/app/pantallaDirec1/page.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import DirectoryScreen from "@/components/PantallaBaseDirectorio";
-
-const PantallaDirectorio1 = () => {
-  return <DirectoryScreen screenNumber={1} />;
-};
-
-export default PantallaDirectorio1;
